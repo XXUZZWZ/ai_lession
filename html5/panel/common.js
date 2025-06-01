c7ffa87cb4baace5b9735a92c8047ced6720873b
XXUZZWZ <3189315662@qq.com>
2025-06-01 23:17:48 +0800

    改变类名

--- html5/panel/common.js
+++ html5/panel/common.js
@@ -1,30 +1,30 @@
-const panels = document.querySelectorAll(".qq-panel");
+const panels = document.querySelectorAll(".fz-panel");
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
     removeActiveClass();
     //模块化;
-    panel.classList.add("qq-panel_active");
+    panel.classList.add("fz-panel_active");
   });
 });
 function removeActiveClass() {
   panels.forEach((panel) => {
-    panel.classList.remove("qq-panel_active");
+    panel.classList.remove("fz-panel_active");
   });
 }
 
-// const panels = document.querySelectorAll('.qq-panel');
+// const panels = document.querySelectorAll('.fz-panel');
 // panels.forEach(panel => {
 //     // JS 是事件机制的语言
 //     // 每一个元素上监听
 //     panel.addEventListener('click', () => {
 //         console.log('biubiubiu');
 //         removeActiveClasses(); //模块化
-//         panel.classList.add("qq-panel_active");
+//         panel.classList.add("fz-panel_active");
 //     })
 // })
 
 // function removeActiveClasses() {
 //     panels.forEach(panel => {
-//         panel.classList.remove('qq-panel_active');
+//         panel.classList.remove('fz-panel_active');
 //     })
 // }

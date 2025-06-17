5c080c86e29d4a6a257dbfdc841e87a31e4ef429
XXUZZWZ <3189315662@qq.com>
2025-06-18 01:56:17 +0800

    feat(css): 添加 demo21 项目文件
    - 新增 common.css、common.js、common.styl 和 index.html 文件
    - 实现了页面的基本样式和功能
    - 添加了表单提交事件处理函数

---
+++ css/Stylus/demo21/common.js
@@ -0,0 +1,9 @@
+const addItem = document.querySelector('.add-item')//from
+const itemsList = document.querySelector('.paltes') // 列表
+
+function addItem(e){
+  e.preventDefault();
+  alert("阻止了")
+}
+
+addTtems.addEventListener('submit',addItem)

b9e04936ccbd349b8f862c12a40a98bc32289638
XXUZZWZ <3189315662@qq.com>
2025-06-21 22:49:44 +0800

    feat(write_code): 新增手写 new 方法代码及说明
    - 添加了 es5 和 es6 两种实现方式的 objectFactory 函数
    - 实现了自定义 new 方法的核心逻辑，包括：
      - 创建空对象
      - 将空对象的原型指向构造函数的原型
      - 将构造函数的作用域绑定到空对象
      - 处理返回值为对象的情况
    - 添加了 Person 构造函数作为示例
    - 编写了 readme.md 文件，对代码进行了详细说明

---
+++ javaScript/wtite_code/1.js
@@ -0,0 +1,55 @@
+// /**
+//  * new方法es5写法
+//  */
+// function objectFactory() {
+//   let obj = new Object();
+//   // 类数组没有数组方法，所以用call来写
+//   var Constructor = Array.prototype.shift.call(arguments); // 构造函数
+//   Constructor.apply(obj, arguments);
+//   obj.__proto__ = Constructor.prototype;
+//   var ret = Constructor.apply(obj, arguments);
+//   //  ret||obj 处理ret为null的情况，仍然会返回obj
+//   // typeof ret === "object"处理返回值式简单数据类型的情况
+//   return typeof ret === "object" ? ret || obj : obj;
+// }
+
+/**
+ * es6写法
+ * @param {any} Constructor
+ * @param  {...any} args
+ * @returns 创建的实例
+ */
+function objectFactory(Constructor, ...args) {
+  var Constructor = Array.prototype.shift.call(arguments); // 构造函数
+  Constructor.apply(obj, arguments);
+  obj.__proto__ = Constructor;
+  var ret = Constructor.apply(obj, args);
+  return typeof ret === "object" ? ret || obj : obj;
+}
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  // 返回复杂类型就会替代本该返回的
+  // this.name = name;
+  // this.age = age;
+  // return {
+  //   name: name,
+  //   age: 18,
+  //   label: "hello",
+  // };
+  // 如果返回简单数据类型 会被忽略
+  // return 1;
+  return null;
+}
+Person.prototype.sayHi = function () {
+  console.log(`my is ${this.name} ,my age is ${this.age}`);
+};
+let person = new Person("p1", 18);
+// person.sayHi();
+console.log(person);
+let person2 = objectFactory(Person, "jtmd", 18);
+console.log(person2);
+person2.sayHi();
+console.log(person2 instanceof Person);
+// new Person("jtmd", 18)---> function [[consturct]] ->{}&&this->{} ->[[call]] --> {}.__proto__ --> Constructor.prototype----return {}

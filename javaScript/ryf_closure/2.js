1466298b6d787457430f1760cf62754abbb5f8d5
XXUZZWZ <3189315662@qq.com>
2025-06-23 22:32:43 +0800

    feat(javaScript): 闭包示例代码更新
    - 在 f2 函数中增加对变量 n 的操作，展示闭包可以修改外部函数变量
    - 添加 f2 函数调用示例，说明闭包如何保持变量状态
    - 注释解释闭包的特性：变量在函数外部不可访问，但在闭包内部保持存活

--- javaScript/ryf_closure/2.js
+++ javaScript/ryf_closure/2.js
@@ -5,6 +5,18 @@ function f1() {
   var n = 999; // 自由变量
   function f2() {
     console.log(n);
+    n += 1;
+    return n;
   }
   return f2;
 }
+
+var f2 = f1();
+f2();
+// 执行完后变量没有立即被销毁，而是被保存在函数f2中，当f2被调用时，变量n才被回收
+// console.log(n);
+console.log(f2());
+
+/*
+
+*/

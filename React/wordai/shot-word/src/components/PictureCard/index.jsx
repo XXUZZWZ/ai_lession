c9cdcdbb6576496950bd8ab659741d1ced57bc7c
XXUZZWZ <3189315662@qq.com>
2025-07-03 22:54:39 +0800

    refactor(React/wordai): 优化 PictureCard 组件并添加新功能
    - 在 PictureCard 组件中添加 props 参数，接收 uploadImage 和 word 属性
    - 实现图片上传后的预览功能
    - 添加将图片数据传递给父组件的功能
    - 优化代码，使用可选链操作符处理文件上传

--- React/wordai/shot-word/src/components/PictureCard/index.jsx
+++ React/wordai/shot-word/src/components/PictureCard/index.jsx
@@ -1,16 +1,19 @@
 import React,{useState} from 'react';
 import './style.css';
 
-const PictureCard = () => {
+const PictureCard = (props) => {
+  console.log(props," ///props");
+  const {uploadImage,word} = props;
+  console.log(uploadImage," ///uploadImage");
   const [imgPreview, setImgPreview] = useState('https://res.bearbobo.com/resource/upload/W44yyxvl/upload-ih56twxirei.png');
-  const [word,setWord] = useState('');
+
   const updateImageData = (e) => {
     // html5的图片上传
     // const file = e.target.files[0];
     const file = e.target.files?.[0];
     // 代替下面这种写法
     // const file = e.target.files?e.target.files[0]:null;
-    // ?. 可选链操作符：当 files 为 null 或 undefined 时避免报错，直接返回 undefined 亮点
+    // ?. 可选链操作符：当 files 为 null 或 undefined 时避免报错，直接返回 undefined ！！！！es6 新特性
     if(!file) return ;
     console.log(file);
     // 图片预览 
@@ -22,7 +25,10 @@ const PictureCard = () => {
         // console.log(e.target.result);
         // 响应式业务
         setImgPreview(e.target.result);
-
+        // 如何将图片数据交给父组件？
+        // 通过父组件给子组件的回调函数来传递数据
+        uploadImage(e.target.result);
+        resolve(e.target.result)
       }
     })
   }
